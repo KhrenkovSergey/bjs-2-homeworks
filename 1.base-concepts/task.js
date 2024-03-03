242d017625686231d7a4cb71add551cdf4974011
Sergey <just1play@mail.ru>
2024-03-03 15:31:38 +0300

    functions fixed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,21 +2,16 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  if (d < 0){
-    return arr;
-  } else if (d === 0) {
+  if (d === 0) {
     let x = -b / (2 * a);
     arr.push(x);
-    return arr;
-  } else {
+  } else if (d > 0) {
     let x = (-b + Math.sqrt(d)) / (2 * a);
     let y = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x, y);
-    return arr;
   } 
+  return arr;
 }
-
-console.log(solveEquation(2, 42, 8));
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
@@ -24,9 +19,5 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let S = amount - contribution;
   let payment = S * (p + (p / (((1 + p) ** countMonths) - 1)));
   let totalAmount = payment * countMonths;
-  return totalAmount.toFixed(2);
+  return +totalAmount.toFixed(2);
 }  
-
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
-console.log(calculateTotalMortgage(10, 1000, 50000, 12));
-console.log(calculateTotalMortgage(10, 0, 20000, 24));
