cc4d221f1e5633bc498572cd43288506569706ec
Sergey <just1play@mail.ru>
2024-05-02 22:46:08 +0300

    task completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -25,12 +25,12 @@ class Triangle {
         }
     }
 
-    get perimetr() {
+    get perimeter() {
         return this.a + this.b + this.c;
     }
 
     get area() {
-        let p = this.perimetr / 2;
+        let p = this.perimeter / 2;
         return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3))
     }
 }
@@ -40,7 +40,7 @@ function getTriangle (a, b, c) {
         return new Triangle(a, b, c)
     } catch (error) {
         return {
-            get perimetr() {
+            get perimeter() {
                 return "Ошибка! Треугольник не существует"
             },
             get area() {
